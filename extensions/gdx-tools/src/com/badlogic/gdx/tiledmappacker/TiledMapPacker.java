c01e244afaf9dad8a9b3e307584a2cf07bb7fe64
litwak913 <litwak913@qq.com>
2026-07-10 23:15:06 +0800

    fix lf

--- extensions/gdx-tools/src/com/badlogic/gdx/tiledmappacker/TiledMapPacker.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tiledmappacker/TiledMapPacker.java
@@ -978,7 +978,7 @@ public class TiledMapPacker {
 
 		TiledMapPacker packer = new TiledMapPacker(packerSettings);
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(100,50);
+		config.setWindowedMode(100, 50);
 		config.setTitle("TiledMapPacker");
 		new Lwjgl3Application(new ApplicationListener() {
 
